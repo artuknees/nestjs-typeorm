d859ac096d7691dc6d0a4a92420032be8cd49de8
artuknees <artuknees@gmail.com>
2024-06-20 20:23:55 -0300

    native database connection

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,10 +1,11 @@
 import { Controller, Get, Param, Query } from '@nestjs/common';
 import { AppService } from './app.service';
+import { ApiTags } from '@nestjs/swagger';
 
+@ApiTags('General')
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
-
   @Get()
   getHello(): string {
     return this.appService.getHello();
@@ -18,5 +19,10 @@ export class AppController {
   @Get('/ruta/')
   hello() {
     return 'con /sas/';
+  }
+
+  @Get('tasks')
+  tasks() {
+    return this.appService.getTasks();
   }
 }
--- src/database/database.module.ts
+++ src/database/database.module.ts
@@ -1,16 +1,32 @@
 import { Module, Global } from '@nestjs/common';
+import { Client } from 'pg';
 
 const API_KEY = '12345634';
 const API_KEY_PROD = 'PROD1212121SA';
 
-@Global()
+const client = new Client({
+  user: 'postgres',
+  host: 'localhost',
+  database: 'platzi_store',
+  password: '2209',
+  port: 5432,
+});
+
+client.connect();
+
+@Global() // es un modulo global. cualquier inyectable sera visto por los demas servicios.
 @Module({
   providers: [
     {
       provide: 'API_KEY',
       useValue: process.env.NODE_ENV === 'prod' ? API_KEY_PROD : API_KEY,
     },
+    {
+      // Comparto el cliente de postgres aca
+      provide: 'PG',
+      useValue: client,
+    },
   ],
-  exports: ['API_KEY'],
+  exports: ['API_KEY', 'PG'],
 })
 export class DatabaseModule {}
